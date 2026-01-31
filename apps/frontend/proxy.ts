b8f70f5a5cc68646162f1f547e7f125e4f114dcc
StefanoCodes <stefanovidmarbusiness@gmail.com>
2026-01-31 17:41:25 +0100

    feat:added proxy file to optmistically redirect users to their destination based on the sessionCookie.

---
+++ apps/frontend/proxy.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
+
+export async function proxy(request: NextRequest) {
+	const sessionCookie = getSessionCookie(request);
+
+	// This is the recommended approach to optimistically redirect users
+	// also do the auth check in each page this is just an optimistic redirect NOT secure.
+	if (!sessionCookie) {
+		return NextResponse.redirect(new URL("/", request.url));
+	}
+
+	return NextResponse.next();
+}
+
+export const config = {
+	matcher: ["/dashboard"], // Specify the routes the middleware applies to
+};
