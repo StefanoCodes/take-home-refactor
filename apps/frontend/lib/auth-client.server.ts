2348818e383a6d9e5180ba0a855b9f5636761bf9
StefanoCodes <stefanovidmarbusiness@gmail.com>
2026-02-01 19:31:25 +0100

    fix:small bug in the CLS shift when a modal is open.

--- apps/frontend/lib/auth-client.server.ts
+++ apps/frontend/lib/auth-client.server.ts
@@ -3,7 +3,7 @@ import { betterAuth } from "better-auth";
 import { Pool } from "pg";
 import { nextCookies } from "better-auth/next-js";
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DATABASE_URL!;
 
 if (!connectionString) {
 	throw new Error("DATABASE_URL environment variable is required");
